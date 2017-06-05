78188cf9deb6c36351e98b4c63f189b2355e72c0
Chris Wiegman <cwiegman@ufl.edu>
2017-06-05 12:59:44 -0400

    Setup default prompt and prepare for plugin generator

---
+++ generators/app/index.js
@@ -0,0 +1,13 @@
+let Base  = require('extendable-yeoman').Base;
+let chalk = require('chalk');
+
+module.exports = Base.extend(
+	{
+		notify: function () {
+			// replace it with a short and sweet description of your generator
+			this.log(chalk.magenta('Invoke a subgenerator to get started!'));
+			this.log('Available Modules:');
+			this.log(chalk.green('\tyo wp:plugin'));
+		}
+	}
+);
