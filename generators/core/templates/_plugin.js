3b7b547fc35682278d69a4ddc7d2b505a061f0b6
Chris Wiegman <cwiegman@ufl.edu>
2018-07-05 10:19:01 -0400

    Cleanup for a simple test environment

--- generators/core/templates/_plugin.js
+++
@@ -1,7 +0,0 @@
-/**
- * <%if ( '' !== unitName ) { %><%= unitName %> <% } %><%= pluginName %>
- * <%= projectHome %>
- *
- * Copyright (c) <%= new Date().getFullYear() %> <%= authorName %>
- * Licensed under the GPLv2+ license.
- */
